929a33e6e69cb5302cdca722342769a65ff3e942
dannabelleli <belleli.danna@gmail.com>
2023-05-11 10:41:43 -0700

    added a function to display the current day's weather and icon

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,12 @@
 var btnSearch = document.querySelector('#btn-search');
 var cityName = document.querySelector('#city');
 var weatherApiKey = 'd16e7b8f62c28e3ded8c3dff53bf6a56';
+var dayTemp = document.querySelector('#day_temp');
+var dayHeader = document.querySelector('#day_header');
+var dayWind = document.querySelector('#day_wind');
+var dayHumidity = document.querySelector('#day_humidity');
+let headerIcon = document.querySelector('.weather-icon');
+
 var lon;
 var lat;
 
@@ -24,15 +30,28 @@ function convertCityPosition() {
 // get weather function
 function getWeather() {
     // weather url
-    var weatherUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherApiKey}`;
+    var weatherUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${weatherApiKey}`;
 
     fetch(weatherUrl)
         .then(function (response) {
             return response.json();
         }).then(function (data) {
             console.log(data);
+            displayWeatherData(data);
         })
 
 }
+
+function displayWeatherData(weatherData) {
+    dayHeader.textContent = weatherData.city.name + ' (' + (new Date(weatherData.list[0].dt_txt)).toDateString() + ')';
+    dayTemp.textContent = 'Temp: ' + weatherData.list[0].main.temp;
+    dayWind.textContent = 'Wind: ' + weatherData.list[0].wind.speed;
+    dayHumidity.textContent = 'Humidity: ' + weatherData.list[0].main.humidity;
+    var icon = weatherData.list[0].weather[0].icon;
+    headerIcon.innerHTML = `<img src="https://openweathermap.org/img/wn/${icon}.png">`;
+}
+
+
+
 
 btnSearch.addEventListener('click', convertCityPosition);
